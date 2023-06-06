da8723bc7ea8594b75864d9580d27cb527de7e2b
Ann-Silje <ann-silje.kirkvik@kartverket.no>
2023-06-06 12:25:44 +0200

    Update doc to new version

--- midgard/__init__.py
+++ midgard/__init__.py
@@ -32,7 +32,7 @@ from pathlib import Path as _Path
 # Version of Midgard.
 #
 # This is automatically set using the bumpversion tool
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 
 
 # Authors of Midgard.
