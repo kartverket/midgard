2204d4af05d15759de0ca35342209181f6b0db41
Ingrid Fausk <ingrid.fausk@kartverket.no>
2025-09-17 13:55:33 +0200

    Added as_dataset function

--- midgard/parsers/bernese_slr_plt.py
+++ midgard/parsers/bernese_slr_plt.py
@@ -13,12 +13,16 @@ Description:
 Reads data from files in Bernese PLT format.
 
 """
+
 # Standard library imports
 from typing import Any, Dict
 
 # Midgard imports
 from midgard.parsers import LineParser
 from midgard.dev import plugins
+from midgard.data import dataset
+from midgard.data.time import Time
+from midgard.dev import log
 
 
 @plugins.register
@@ -72,8 +76,7 @@ class BerneseSlrPltParser(LineParser):
             autostrip=True,
             comments="#",
             names=("station", "domes", "sat_prn", "epoch", "residual", "azi", "ele"),
-            delimiter=(5, 10, 7, 19, 9, 10, 8),
-            dtype=("U4", "U9", "U3", "f", "f", "f", "f"),
+            dtype=("U4", "U9", "U4", "f", "f", "f", "f"),
             skip_header=4,
         )
 
@@ -99,3 +102,53 @@ class BerneseSlrPltParser(LineParser):
                     float(self.data[key][idx])
                 )
         return dict_
+
+    #
+    # WRITE DATA
+    #
+    def as_dataset(self) -> "Dataset":
+        """Store slr residual data in a dataset
+
+        Returns:
+            Midgard Dataset where residual data are stored with following fields:
+
+
+        | Field               | Type              | Description                                                 |
+        | :------------------ | :---------------- | :---------------------------------------------------------- |
+        | station             | str               | Station name                                                |
+        | residual            | numpy.ndarray     | Residual, difference between orbit and SLR measurement      |
+        | domes               | str               | Domes number of station                                     |
+        | elevation           | numpy.ndarray     | Elevation from receiver                                     |
+        | azimuth             | numpy.ndarray     | Azimuth from receiver                                       |
+        | sat_prn             | str               | Name of satellite                                           |
+        | epoch               | Time              | Time of observation, modified julian date                   |
+        | time                | Time              | Time of observation, datetime                               |
+
+        """
+
+        dset = dataset.Dataset(num_obs=len(self.data["epoch"]))
+        dset.meta.update(self.meta)
+
+        dset.add_time(
+            "time",
+            val=[
+                Time(int(v // 1), val2=v % 1, fmt="mjd", scale="utc")
+                for v in self.data["epoch"]
+            ],
+            fmt="mjd",
+            scale="utc",
+        )
+
+        # Loop over Dataset fields
+        for field, val in self.data.items():
+
+            if field in ["station", "domes", "sat_prn"]:
+                dset.add_text(field, val=val)
+
+            elif field in ["epoch", "residual", "azi", "ele"]:
+                dset.add_float(field, val=val)
+
+            else:
+                log.fatal(f"Unknown field {field}")
+
+        return dset
